0e233ae35b1d0e6604c76e0dc39c47870bc87088
Rohanator9000 <rkhajuria99@gmail.com>
2015-10-10 20:13:16 -0400

    PreTeleop Update
    Updated values from motor first, and then scales it
    Changes from trigger input to bumper input
    Fixes bugs:
    Correctly updates scales
    Resets motor values to 0

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PreTeleop.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PreTeleop.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
+import static java.lang.Math.abs;
+
 /**
  * Created by rkhaj on 10/9/2015.
  * This is a basic teleop that we are going to use for testing purposes.
@@ -41,58 +43,67 @@ public class PreTeleop extends OpMode {
     public void loop() {
 
         /*
-        Creates variables for gamepad input and assigns values to them
+        Creates variables for gamepad input
         "Ver"=vertical
-
         Creates variable for mode
         */
-        float leftVer = -gamepad1.left_stick_y;
-        float rightVer = gamepad1.right_stick_y;
+        float leftVer;
+        float rightVer;
+        String mode;
 
-        String mode = "Regular";
+        /*
+        Updates the power variable only if its absolute value is greater than 0.08
+        This prevents the motors from burning out
+        */
+        if (abs(gamepad1.left_stick_y)>0.08) {
+            leftVer = -gamepad1.left_stick_y;
+        } else {
+            leftVer = 0;
+        }
+        if (abs(gamepad1.right_stick_y)>0.08) {
+            rightVer = gamepad1.right_stick_y;
+        } else {
+            rightVer = 0;
+        }
 
         /*
         Scales gamepad values depending on buttons pressed
         This allows the driver to have more precision
-
         Also updates the mode display
         */
-        if (gamepad1.left_trigger == 1){
-            leftVer *= 0.75;
-            rightVer *= 0.75;
-            mode = "slow";
-        } else if (gamepad1.left_trigger == 1 && gamepad1.right_trigger == 1) {
+        if (gamepad1.left_bumper && gamepad1.right_bumper){
+            leftVer *= 0.2;
+            rightVer *= 0.2;
+            mode = "slower";
+        } else if (gamepad1.left_bumper) {
             leftVer *= 0.5;
             rightVer *= 0.5;
-            mode = "slower";
+            mode = "slow";
+        } else {
+            mode = "Regular";
         }
 
         /*
-        Updates motor power only if input value is greater than 0.08
-        This prevents motors from breaking
+        Updates motor power
         */
-        if (leftVer>0.08) {
-            lfMotor.setPower(leftVer);
-            lbMotor.setPower(leftVer);
-        }
-        if (rightVer>0.08) {
-            rfMotor.setPower(rightVer);
-            rbMotor.setPower(rightVer);
-        }
+        lfMotor.setPower(leftVer);
+        lbMotor.setPower(leftVer);
+        rfMotor.setPower(rightVer);
+        rbMotor.setPower(rightVer);
 
         /*
         Display:
-        the mode we're in (regular, slow, or slower
+        the mode we're in (regular, slow, or slower)
         left joystick output
         right joystick output
         left trigger output
         right trigger output
          */
         telemetry.addData("Mode", mode);
-        telemetry.addData("Left Vertical Ouput", leftVer);
-        telemetry.addData("Right Vertical Output", rightVer);
-        telemetry.addData("Left Trigger", gamepad1.left_trigger);
-        telemetry.addData("Right Trigger", gamepad1.right_trigger);
+        telemetry.addData("Left Vertical Ouput", lfMotor.getPower());
+        telemetry.addData("Right Vertical Output", rfMotor.getPower());
+        telemetry.addData("Left Bumper", gamepad1.left_bumper);
+        telemetry.addData("Right Bumper", gamepad1.right_bumper);
     }
 
     @Override
