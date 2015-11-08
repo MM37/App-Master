063b841faeacedee83e1e44067f4d869cfe97cd3
Rohanator9000 <rkhajuria99@gmail.com>
2015-11-07 20:27:51 -0500

    Fixed Autonomous Encoder Error
    set motor channel modes which i forgot to add earlier

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousRepairZoneRedEncoder.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousRepairZoneRedEncoder.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
@@ -88,6 +89,11 @@ public class AutonomousRepairZoneRedEncoder extends LinearOpMode {
         rbMotor = hardwareMap.dcMotor.get("rbMotor");
 
         colorSensor = hardwareMap.colorSensor.get("colorSensor");
+
+        lfMotor.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        lbMotor.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        rfMotor.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        rbMotor.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
 
         waitOneFullHardwareCycle();
         waitForStart();
